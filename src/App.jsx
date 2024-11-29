d738bf4f5adbd1f31f12ce90b08ea3363252d30c
abdur rhaman babu <abdurrahmanbab74@gamil.com>
2024-11-29 15:00:37 +0600

    delete coffee form server

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ const App = () => {
   const coffees = useLoaderData();
   return (
     <div>
-      <h1>Coffees: {coffees.length}</h1>
+      <h1 className="text-center font-bold text-4xl mt-10 mb-5">Coffees: {coffees.length}</h1>
       <div className="grid md:grid-cols-2 lg:grid-cols-3 mx-auto max-w-7xl gap-5">
         {coffees.map((coffee) => (
           <CoffeeCard key={coffee._id} coffee={coffee} />
--- src/components/CoffeeCard.jsx
+++ src/components/CoffeeCard.jsx
@@ -1,27 +1,72 @@
 import { FaEdit, FaEye } from "react-icons/fa";
 import { MdDelete } from "react-icons/md";
+import Swal from "sweetalert2";
 
-const CoffeeCard = ({coffee}) => {
-    const {name,chef,category,photo} = coffee;
-    return (
-        <div>
-            <div className="flex items-center gap-5 border p-5 shadow-lg">
-                <div className="h-[200px] w-[150px]">
-                    <img className="w-full h-full" src={photo} alt="" />
-                </div>
-                <div className="flex-grow">
-                    <p><span className="font-bold">Name: </span>{name}</p>
-                    <p><span className="font-bold">Chef: </span>{chef}</p>
-                    <p><span className="font-bold">Price: </span>{category}</p>
-                </div>
-                <div className="flex flex-col gap-5">
-                    <button><FaEye size={35} /></button>
-                    <button><FaEdit size={35} /></button>
-                    <button><MdDelete size={35}/></button>
-                </div>
-            </div>
+const CoffeeCard = ({ coffee }) => {
+  const { _id, name, chef, category, photo } = coffee;
+  const handleDeleteCoffee = (id) => {
+    console.log(id);
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        fetch(`http://localhost:3500/coffees/${id}`,{
+            method:'DELETE'
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            console.log(data);
+            if (data.deletedCount > 0) {
+                Swal.fire({
+                  title: "Deleted!",
+                  text: "Your Coffee has been deleted.",
+                  icon: "success"
+                });
+            }
+          });
+      }
+    });
+  };
+  return (
+    <div>
+      <div className="flex items-center gap-5 border p-5 shadow-lg">
+        <div className="h-[200px] w-[150px]">
+          <img className="w-full h-full" src={photo} alt="" />
         </div>
-    );
+        <div className="flex-grow">
+          <p>
+            <span className="font-bold">Name: </span>
+            {name}
+          </p>
+          <p>
+            <span className="font-bold">Chef: </span>
+            {chef}
+          </p>
+          <p>
+            <span className="font-bold">Price: </span>
+            {category}
+          </p>
+        </div>
+        <div className="flex flex-col gap-5">
+          <button>
+            <FaEye size={35} />
+          </button>
+          <button>
+            <FaEdit size={35} />
+          </button>
+          <button onClick={() => handleDeleteCoffee(_id)}>
+            <MdDelete size={35} />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default CoffeeCard;
